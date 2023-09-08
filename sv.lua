98adb5e29d9c26be490ff1d160073a4feb4217f4
Nikke77 <140919460+Nikke77@users.noreply.github.com>
2023-09-08 16:07:02 +0300

    Update sv.lua

--- sv.lua
+++ sv.lua
@@ -10,8 +10,8 @@ ESX.RegisterUsableItem('lapio', function(source)
     TriggerClientEvent('esx_n_digging:client:start', source)
 end)
 
-local item = items[math.random(1,#items)]
 RegisterNetEvent("esx_n_digging:server:givereward", function ()
+    local item = items[math.random(1,#items)]
     local source = source
     local player = ESX.GetPlayerFromId(source)
     player.addInventoryItem(item, 1)
